58bfd65f99598d84db4965f2c11db6f48144f106
wlrojas <162857504+wlrojas@users.noreply.github.com>
2024-06-10 19:03:44 +0000

    d

--- MyApplication/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MyApplication',
-  webDir: 'www'
-};
-
-export default config;
